afa89b9824b9225bf93f69189ff4f8c5e06acf2a
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2025-03-01 16:26:02 +0500

    connect mongodb and create model

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongooes from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
+});
+
+dotenv.config();
+
+mongooes.connect(process.env.MONGO_URI, ).then(() => {
+    console.log('Database connected');
+}).catch((err) => {
+    console.log(err);
 });
